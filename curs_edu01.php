7e8ea5dbf62e9cd686805a37036c8e8564c045cf
Alexx280 <Alexx280@yandex.ru>
2016-03-14 15:27:47 +0600

    Добавлен код выведения красивых окон через JS, тесирование.

--- curs_edu01.php
+++ curs_edu01.php
@@ -6,7 +6,7 @@ include_once 'head_edu.tpl';
 <!DOCTYPE HTML>
 <html>
 <head>
-
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 </head>
 <div id="edu" >
     <p class="animated flipInX tq"> 1. Понятие информационной безопасности.</p><br>
@@ -33,12 +33,12 @@ include_once 'head_edu.tpl';
     </div>
     <div class="butt2">
     <a href="curs_edu02.php">
-        <button type="button" class="btn btn-success btn-lg btn3d"><span class="glyphicon glyphicon-ok"></span> Вперёд</button>
+        <button type="button" id="win" class="btn btn-success btn-lg btn3d"><span class="glyphicon glyphicon-ok"></span> Вперёд</button>
     </a>
     </div>
-    <div class="butt3">
-        <a href="index.php">
-            <button type="button" class="btn btn-danger btn-lg btn3d" title="Перейти на начальную страницу"><span class="glyphicon glyphicon-remove"></span></button>
+    <div class="butt3" id="win1">
+        <a href="#">
+            <button type="button" id="win" class="btn btn-danger btn-lg btn3d" title="Перейти на начальную страницу" onclick='messageShow()'><span class="glyphicon glyphicon-remove"></span></button>
         </a>
     </div>
     <div class="progress top-bar" >
@@ -47,5 +47,27 @@ include_once 'head_edu.tpl';
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+/*    function messageShow() {
+
+        //$('#edu').append('Этот текст добавлен с помощью jQuery');
+        $('#edu').html( "" +
+        "<div id='centr-w'>" +
+            "<div id='centr' style='text-align: center;  '  >" +
+                "Введите пробный пароль <br>" +
+                "<form method='post' action='#'>" +
+                    "<input type='text' name='parol' size='20' style='text-align: center; color: red '/><br><br>"+
+                    "<div style='text-align: center;'><input type='submit' value='Проверить' class='table-form'/><br></div>" +
+                "</form>"+
+            "<br><a href='curs_edu01.php'><button type='button' class='btn btn-success btn-lg btn3d' style='width: 120px'> Назад</button></a>" +
+            "</div>" +
+        "</div>");
+    }
+*/
+</script>
+<?php
+
+?>
 
 </html>
