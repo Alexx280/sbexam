8519645c5817252e853efd101469e54a40978f5e
Alexx280 <gs455@mail.ru>
2016-05-16 15:19:59 +0600

    Сделал чтобы не логинится несколько раз

--- curs_edu20.php
+++ curs_edu20.php
@@ -2,6 +2,9 @@
 session_start();
 include_once 'funk.php';
 include_once 'head_edu.tpl';
+if ($_SESSION['lama_id'] != '') {
+    $file = '1.php';}
+else {$file = 'test.php';};
 ?>
 
 <!DOCTYPE HTML>
@@ -10,10 +13,15 @@ include_once 'head_edu.tpl';
     <p class="animated flipInX tq"> Контроль знаний </p><br>
     <p class="tf animated bounceInUp delay1">
         Краткое описание правил информационной безопасности завершено.<br>
-    Предлагаем Вам проверить свои знания.<br></p>
+        Проверте свои знания.<br>
+        Для успешной сдачи зачёта необходимо ответить  <br>правильно минимум на 7 из 10 вопросов.<br>
+        В каждом вопросе может быть несколько ответов.<br>
+        Вернуться на предыдущитй вопрос будет невозможно.<br>
+        На каждый вопроc выделено по 2 минуты<br>
+        <br><br></p>
 
     <div class="butt1">
-        <a href="test.php">
+        <a href="<?php echo $file?> ">
             <button type="button" class="btn btn-success btn-lg btn3d" style="width: 120px"> Тест </button>
         </a>
     </div>
